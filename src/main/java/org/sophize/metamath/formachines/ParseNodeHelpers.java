ecf34e9d8c31bb55b06518f9cc3978e0b8d9a7a4
Abhishek Chugh <achugh89@gmail.com>
2020-06-27 18:55:03 +0530

    More helper functions for parseNode

--- src/main/java/org/sophize/metamath/formachines/ParseNodeHelpers.java
+++ src/main/java/org/sophize/metamath/formachines/ParseNodeHelpers.java
@@ -14,7 +14,13 @@ public class ParseNodeHelpers {
   private static Set<String> NUMBER_LABELS =
       Set.of("cdc", "cc0", "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10");
   private static Set<String> SPECIAL_SET_STATEMENTS = Set.of("cn", "cn0", "cc", "cr");
-  private static Map<String, String> LABEL_REPLACEMENTS = Map.of("wcel", "e");
+  private static Map<String, String> LABEL_REPLACEMENTS =
+      Map.ofEntries(
+          Map.entry("wcel", "in"),
+          Map.entry("wceq", "eq"),
+          Map.entry("caddc", "p"),
+          Map.entry("cmul", "t"),
+          Map.entry("cprime", "prm"));
 
   public static String getLabel(ParseNode node) {
     String special = specialLabel(node);
@@ -34,17 +40,69 @@ public class ParseNodeHelpers {
     return labelBuilder.toString();
   }
 
-  static String asString(ParseNode node, String startTag) {
-    return String.join(" ", getLabel(node), startTag, asString(node), "$.");
+  public static boolean isNumberExpression(ParseNode node) {
+    return NUMBER_LABELS.contains(node.stmt.getLabel());
   }
 
-  static String asString(ParseNode node) {
+  public static boolean isSumOrNumberExpression(ParseNode node) {
+    if (isNumberExpression(node)) return true;
+    if (!node.stmt.getLabel().equals("co")) return false;
+    if (!node.child[2].stmt.getLabel().equals("caddc")) return false;
+    return isSumOrNumberExpression(node.child[0]) && isSumOrNumberExpression(node.child[1]);
+  }
+
+  public static boolean isSumOrProductOrNumberExpression(ParseNode node) {
+    if (isNumberExpression(node)) return true;
+    if (!node.stmt.getLabel().equals("co")) return false;
+    if (!node.child[2].stmt.getLabel().equals("caddc")
+        && !node.child[2].stmt.getLabel().equals("cmul")) return false;
+    return isSumOrProductOrNumberExpression(node.child[0])
+        && isSumOrProductOrNumberExpression(node.child[1]);
+  }
+
+  public static boolean isSumExpressionStatement(ParseNode node) {
+    if (!node.stmt.getLabel().equals("wceq")) return false;
+    return isSumOrNumberExpression(node.child[0]) && isSumOrNumberExpression(node.child[1]);
+  }
+
+  public static boolean isSumOrProductExpressionStatement(ParseNode node) {
+    if (!node.stmt.getLabel().equals("wceq")) return false;
+    return isSumOrProductOrNumberExpression(node.child[0])
+        && isSumOrProductOrNumberExpression(node.child[1]);
+  }
+
+  public static NumberRepresentation getValue(ParseNode node) {
+    if (isNumberExpression(node)) return NumberRepresentation.fromParseNode(node);
+    if (node.stmt.getLabel().equals("co")) {
+      if (node.child[2].stmt.getLabel().equals("caddc")) {
+        var op1 = getValue(node.child[0]);
+        var op2 = getValue(node.child[1]);
+        if (op1 == null || op2 == null) return null;
+        return new NumberRepresentation(op1.number + op2.number);
+      } else if (node.child[2].stmt.getLabel().equals("cmul")) {
+        var op1 = getValue(node.child[0]);
+        var op2 = getValue(node.child[1]);
+        if (op1 == null || op2 == null) return null;
+        return new NumberRepresentation(op1.number * op2.number);
+      }
+    }
+    return null;
+  }
+
+  static String asString(ParseNode node, String label, String startTag) {
+    return String.join(" ", label, startTag, asStatement(node), "$.");
+  }
+
+  static String asStatement(ParseNode node) {
+    return "|- " + asExpression(node);
+  }
+
+  public static String asExpression(ParseNode node) {
     var formula = node.convertToFormula();
-    return "|- "
-        + Arrays.stream(formula.getSym())
-            .skip(1)
-            .map(Sym::toString)
-            .collect(Collectors.joining(" "));
+    return Arrays.stream(formula.getSym())
+        .skip(1)
+        .map(Sym::toString)
+        .collect(Collectors.joining(" "));
   }
 
   private static String specialLabel(ParseNode node) {
@@ -55,10 +113,12 @@ public class ParseNodeHelpers {
     if (SPECIAL_SET_STATEMENTS.contains(label)) {
       return node.stmt.getFormula().getSym()[1].toString();
     }
-    if (label.equals("wbr")) {
+    if (label.equals("wbr") || label.equals("co")) {
       var leftSide = getLabel(node.child[0]);
       var rightSide = getLabel(node.child[1]);
-      return leftSide + DIVIDER + node.child[2].stmt.getLabel() + DIVIDER + rightSide;
+      var center = node.child[2].stmt.getLabel();
+      center = LABEL_REPLACEMENTS.getOrDefault(center, center);
+      return DIVIDER + leftSide + DIVIDER + center + DIVIDER + rightSide + DIVIDER;
     }
     return null;
   }
